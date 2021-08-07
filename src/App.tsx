5e455a95558fe91e3cb0940697e8935a4fa7096c
ookuboc5399 <ankle0614@gmail.com>
2021-08-07 11:45:02 +0900

    0807

---
+++ src/App.tsx
@@ -0,0 +1,27 @@
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import LinePlot from "./components/LinePlot";
+import PiePlot from "./components/PiePlot";
+import BarPlot from "./components/BarPlot";
+import RadarPlot from "./components/RadarPlot";
+import BubblePlot from "./components/BubblePlot";
+import MaterialUI from "./components/MaterialUI";
+// import DryRun from "./components/DryRun";
+
+function App() {
+  return (
+    <div className="App">
+      {/* <LinePlot />
+      <PiePlot />
+      <BarPlot />
+      <RadarPlot />
+      <BubblePlot /> */}
+      <RadarPlot />
+      <MaterialUI />
+      <DryRun />
+    </div>
+  );
+}
+
+export default App;
